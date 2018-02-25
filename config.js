fc22bf60f48a7552a25f9488627a7faf312b927b
Jas <jazjit.singh@gmail.com>
2018-02-25 13:02:11 -0500

    updates

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-post-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://jas:jas@ds247678.mlab.com:47678/blog-post-v2';
 exports.PORT = process.env.PORT || 8080;
