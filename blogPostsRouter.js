b4edfadb630c79ec506e24874aec80762b8f824a
Jas <jazjit.singh@gmail.com>
2018-02-25 12:59:53 -0500

    updates

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const router = express.Router();
+'usee strict';
 
 const bodyParser = require('body-parser');
+const express = require('express');
+
+const router = express.Router();
+
 const jsonParser = bodyParser.json();
 
-const {BlogPosts} = require('./models');
+const {BlogPost} = require('./models');
 
 router.get('/', (req, res) => {
-  res.json(BlogPosts.get());
+  BlogPost.find()
+  .limit(10)
+  .then(blog_posts => {
+    res.json({
+      blog_posts: blog_posts.map (
+        blog_post => blog_post.serialize())
+      });
+    });
 });
 
-router.post('/', jsonParser, (req, res) => {
+router.get('/:id', (req, res) => {
+  BlogPost.findById(req.params.id)
+  .then(blogpost => res.json(blogpost.serialize()))
+  .catch(err =>  {
+    console.error(err)
+    res.status(500).json({ message: 'Internal server error' })
+  })
+});
+
+router.post('/', (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['title', 'content', 'author'];
+  console.log(req.body);
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -21,40 +41,50 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).json(item);
+  let authorFirstName = req.body.author.split(" ")[0];
+  let authorLastName = req.body.author.split(" ")[1];
+  BlogPost.create({
+    title: req.body.title, 
+    content: req.body.content, 
+    author: {firstName: authorFirstName, lastName: authorLastName},
+  })
+  .then(blogpost => res.status(201).json(blogpost.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  })
 });
 
-router.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
-      `Request path id (${req.params.id}) and request body id `
+      `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
-    return res.status(400).send(message);
+    return res.status(400).json({ message: message });
   }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
-    title: req.params.title,
-    content: req.body.content,
-    author: req.body.author
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+
+  BlogPost
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
   });
-  res.status(204).end();
 })
 
 router.delete('/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
-  res.status(204).end();
+    BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
+mongoose.Promise = global.Promise;
+// const {BlogPosts} = require('./models');
+
+
+const { PORT, DATABASE_URL } = require('./config');
+const { BlogPost } = require ('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
-
+const router = express.Router();
 const blogPostsRouter = require('./blogPostsRouter');
 
 app.use(morgan('common'));
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -26,14 +31,20 @@ let server;
 // this function starts our server and returns a Promise.
 // In our test code, we need a way of asynchronously starting
 // our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
     server = app.listen(port, () => {
       console.log(`Your app is listening on port ${port}`);
       resolve(server);
-    }).on('error', err => {
-      reject(err)
+      }).on('error', err => {
+        mongoose.disconnect();
+        reject(err)
+      });
     });
   });
 }
@@ -58,7 +69,7 @@ function closeServer() {
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 };
 
 module.exports = {app, runServer, closeServer};
